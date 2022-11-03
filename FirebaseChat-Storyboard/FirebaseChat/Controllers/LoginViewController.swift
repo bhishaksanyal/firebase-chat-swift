8d6829daa32f6b17f526a97f50453e048a8bc1ac
bhishaksanyal <sanyalbishak93@gmail.com>
2022-11-03 21:57:58 +0530

    Added login register UI

---
+++ FirebaseChat-Storyboard/FirebaseChat/Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  FirebaseChat
+//
+//  Created by Bhishak Sanyal on 03/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ FirebaseChat-Storyboard/FirebaseChat/Controllers/RegisterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewController.swift
+//  FirebaseChat
+//
+//  Created by Bhishak Sanyal on 03/11/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
