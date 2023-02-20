c25bb043ec1ab9b49b911347ff979670c0b3ef73
bhishaksanyal <sanyalbishak93@gmail.com>
2023-02-20 20:07:18 +0530

    Welcome screen animation added.

--- FirebaseChat-Storyboard/FirebaseChat/Controllers/WelcomeViewController.swift
+++ FirebaseChat-Storyboard/FirebaseChat/Controllers/WelcomeViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        runWelcomeAnimation()
+    }
+    
+    func runWelcomeAnimation() {
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+                
+            }
+            charIndex += 1
+        }
     }
 
 
